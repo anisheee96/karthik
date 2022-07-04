d339bc715583464e01148df323e3024b2b65ae5e
anisheee96 <108547376+anisheee96@users.noreply.github.com>
2022-07-04 17:05:05 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,18 @@
+class version1:
+    def v1(self):
+        print("Button")
+        print("text box")
+class version2(version1):
+    def v2(self):
+        print("drop down list")
+if __name__=='__main__':
+    app=version2()
+    app.v1()
+    app.v2()
+    
+        
+
+
+        
+    
+    
